bb5f7310154778fce9059d96c229f49106782492
poorani.v <91772266+6113poorani@users.noreply.github.com>
2024-10-01 18:32:14 +0530

    Delete src/com/zsgs/flight/screens directory

--- src/com/zsgs/flight/screens/bookedtickets/BookedTicketViewModel.java
+++
@@ -1,9 +0,0 @@
-package com.zsgs.flight.screens.bookedtickets;
-
-class BookedTicketViewModel {
-	private BookedTicketScreen view;
-
-	public BookedTicketViewModel(BookedTicketScreen screen) {
-		view = screen;
-	}
-}
